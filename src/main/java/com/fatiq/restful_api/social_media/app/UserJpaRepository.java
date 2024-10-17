b7eefc754216d7d2282ce26d23a777c1dbb3a779
FATIQ HUSSNAIN <fatiqhussnain1@gmail.com>
2024-10-17 19:21:46 +0500

    social media app

---
+++ src/main/java/com/fatiq/restful_api/social_media/app/UserJpaRepository.java
@@ -0,0 +1,5 @@
+package com.fatiq.restful_api.social_media.app;
+
+public interface UserJpaRepository {
+
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO users(id, name, birth_date)
+VALUES (1001, 'fatiq', CURRENT_DATE());
+
+INSERT INTO users(id, name, birth_date)
+VALUES (1002, 'ali', CURRENT_DATE());
+
+INSERT INTO users(id, name, birth_date)
+VALUES (1003, 'muhammad', CURRENT_DATE());
